aeb00c79212fc10fcc6f57d77fbd871a5481b13c
Vishvambruth J T <vishruth.cse@gmail.com>
2025-06-29 05:45:35 +0530

    SauceLabs integration

--- src/main/java/com/ea/SpringBootAppiumUIFramework/libraries/AppiumDriverConfig.java
+++ src/main/java/com/ea/SpringBootAppiumUIFramework/libraries/AppiumDriverConfig.java
@@ -2,13 +2,20 @@ package com.ea.SpringBootAppiumUIFramework.libraries;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class AppiumDriverConfig {
@@ -18,26 +25,60 @@ public class AppiumDriverConfig {
     @Value("${appium.udid}") private String udid;
     @Value("${appium.platformVersion}") private String platformVersion;
     @Value("${appium.serverUrl}") private String appiumServerUrl;
+    @Value("${use.sauce.labs}") private String useSauceLabs;
+    @Value("${sauce.username}") private String sauceUsername;
+    @Value("${sauce.accessKey}") private String sauceAccessKey;
 
     @Bean
     public WebDriver webDriver() {
         try {
-            UiAutomator2Options options = new UiAutomator2Options();
-            options.setPlatformName("Android");
-            options.setDeviceName(deviceName);
-            options.setUdid(udid);
-            options.setPlatformVersion(platformVersion);
-            options.setNewCommandTimeout(Duration.ofSeconds(60));
-            options.setCapability("browserName", "Chrome");
-            options.setCapability("chromedriverExecutable",
-                    "C:\\Eclipse\\worskspace\\SpringBootAppiumUIFramework\\src\\test\\resources\\ChromeDriver\\chromedriver.exe");
+            if (!Boolean.parseBoolean(useSauceLabs)){
+                // Local Appium with emulator
+                UiAutomator2Options options = new UiAutomator2Options();
+                options.setPlatformName("Android");
+                options.setDeviceName(deviceName);
+                options.setUdid(udid);
+                options.setPlatformVersion(platformVersion);
+                options.setNewCommandTimeout(Duration.ofSeconds(60));
+                options.setCapability("browserName", "Chrome");
+                options.setCapability("chromedriverExecutable",
+                        "C:\\Eclipse\\worskspace\\SpringBootAppiumUIFramework\\src\\test\\resources\\ChromeDriver\\chromedriver.exe");
 
-            AndroidDriver driver = new AndroidDriver(new URL(appiumServerUrl), options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(appUrl);
-            return driver;
+                URI uri = URI.create(appiumServerUrl);
+                AndroidDriver driver = new AndroidDriver(uri.toURL(), options);
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.get(appUrl);
+                return driver;
+            }
+            else {
+                // Sauce Labs setup
+                AndroidDriver driver = getWebDriver();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                driver.get(appUrl);
+                return driver;
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to create AndroidDriver for Appium: " + e.getMessage(), e);
         }
+    }
+
+    private AndroidDriver getWebDriver() throws MalformedURLException {
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+        caps.setCapability("appium:platformVersion", platformVersion);
+        caps.setCapability("appium:automationName", "UIAutomator2");
+
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", sauceUsername);
+        sauceOptions.setCapability("accessKey", sauceAccessKey);
+        sauceOptions.setCapability("build", "SpringBootAppium Build");
+        sauceOptions.setCapability("name", "Sauce Labs Tablet Test");
+        sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
+        caps.setCapability("sauce:options", sauceOptions);
+
+        URI uri = URI.create("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+        return new AndroidDriver(uri.toURL(), caps);
     }
 }
